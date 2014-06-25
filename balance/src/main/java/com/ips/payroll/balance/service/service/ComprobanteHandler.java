a1e1fb06903921b51515dd5e9586702cc42f20f6
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-06-25 00:46:21 -0500

    Back to use jaxb

--- balance/src/main/java/com/ips/payroll/balance/service/service/ComprobanteHandler.java
+++ balance/src/main/java/com/ips/payroll/balance/service/service/ComprobanteHandler.java
@@ -1,65 +1,94 @@
 package com.ips.payroll.balance.service.service;
 
+import mx.gob.sat.cfd.x3.ComprobanteDocument;
+import mx.gob.sat.cfd.x3.TUbicacion;
+import mx.gob.sat.cfd.x3.TUbicacionFiscal;
+import mx.gob.sat.nomina.NominaDocument;
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 
-import mx.gob.sat.cfd.x3.ComprobanteDocument;
-
-import org.apache.xmlbeans.XmlCursor;
-import org.apache.xmlbeans.XmlException;
-import org.apache.xmlbeans.XmlObject;
-import org.apache.xmlbeans.XmlString;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ComprobanteHandler 
-	implements XmlHandlerAbstract<ComprobanteDocument>
+public class ComprobanteHandler
+        implements XmlHandlerAbstract<ComprobanteDocument>
 {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(ComprobanteHandler.class);
-	
-	 private Collection<XmlHandlerAbstract < ? >> handlers =
-		        new HashSet<XmlHandlerAbstract< ? >>();
-	 
-	    private static final javax.xml.namespace.QName NOMINA$0 = 
-	            new javax.xml.namespace.QName("http://www.sat.gob.mx/nomina", "Nomina");
 
-	public ComprobanteHandler() {
-		handlers.add(new NominaHandler());
-	}
-	 
-	@Override
-	public Class<ComprobanteDocument> handles() 
-	{
-		return ComprobanteDocument.class;
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(ComprobanteHandler.class);
 
-	@Override
-	public void handle(ComprobanteDocument aRequest) throws IOException 
-	{
-		LOG.debug("handle {}", aRequest.getComprobante().getComplemento().getDomNode().getFirstChild());
-		
-		try {
-			XmlObject myDocument = XmlString.Factory.parse(aRequest.getComprobante().getComplemento().getDomNode());
-//			XmlObject myDocument = XmlObject.Factory.parse(aRequest.getComprobante().getComplemento().getDomNode());
+    private Collection<XmlHandlerAbstract<?>> handlers =
+            new HashSet<XmlHandlerAbstract<?>>();
+
+    private static final javax.xml.namespace.QName NOMINA$0 =
+            new javax.xml.namespace.QName("http://www.sat.gob.mx/nomina", "Nomina");
+
+    public ComprobanteHandler()
+    {
+        handlers.add(new NominaHandler());
+    }
+
+    @Override
+    public Class<ComprobanteDocument> handles()
+    {
+        return ComprobanteDocument.class;
+    }
+
+    @Override
+    public void handle(ComprobanteDocument aRequest) throws IOException
+    {
+        LOG.debug("handle {}", aRequest.getComprobante().getComplemento().getDomNode().getFirstChild());
+
+        try
+        {
+            //Datos exraidos del comprobante XML
+
+            ComprobanteDocument.Comprobante comprobante = aRequest.getComprobante();
+            ComprobanteDocument.Comprobante.Emisor emisor=comprobante.getEmisor();
+            ComprobanteDocument.Comprobante.Emisor.RegimenFiscal rf=emisor.getRegimenFiscalArray(0);//pocision del arreglo
+            ComprobanteDocument.Comprobante.Receptor receptor=comprobante.getReceptor();
+            TUbicacionFiscal domicilioFiscal=emisor.getDomicilioFiscal();
+            TUbicacion domicilioFiscalR=receptor.getDomicilio();
+            TUbicacion expedidoEn=emisor.getExpedidoEn();
+            ComprobanteDocument.Comprobante.Complemento complemento=comprobante.getComplemento();
+            LOG.debug("Nomina {}", complemento.selectChildren(NOMINA$0)[0].schemaType());
+            LOG.debug("Nomina {}", complemento.selectChildren(NOMINA$0)[0].substitute(NOMINA$0,NominaDocument.type).xmlText());
+            LOG.debug("Nomina {}", complemento.substitute(NOMINA$0,NominaDocument.type));
+            LOG.debug("Nomina {}", complemento);
+            NominaDocument nominaDocument=NominaDocument.Factory.parse(complemento.selectChildren(NOMINA$0)[0].toString());
+            NominaDocument.Nomina nomina=nominaDocument.getNomina();
+
+            LOG.debug("Nomina {}", nomina);
+
+            // myDocument = XmlString.Factory.parse(aRequest.getComprobante().getComplemento().getDomNode());
+			XmlObject myDocument = XmlObject.Factory.parse(aRequest.getComprobante().getComplemento().getDomNode());
 //			XmlObject[] myDocuments = XmlObject.Factory.parse((aRequest.getComprobante().getComplemento().toString())).selectChildren(NOMINA$0);
 //			XmlObject myDocument = myDocuments[0];
-			
-			LOG.debug("myDocument {}", myDocument);
-    		
-    		XmlCursor cursor = myDocument.newCursor();
-    	    while (cursor.toNextSelection())
-    	    {
-    	    	LOG.debug("cursor value {}", cursor.getTextValue());
-    	    }
-    		
-			
-			boolean myHandled = false;
+            XmlOptions myXmlOptions = new XmlOptions();
+            myXmlOptions.setDocumentType(NominaDocument.type);
+
+
+            myDocument = XmlObject.Factory.parse(myDocument.getDomNode());
+
+            LOG.debug("myDocument {}", myDocument.xmlText());
+
+            XmlCursor cursor = myDocument.newCursor();
+            LOG.debug("cursor child {}", cursor.toChild(NOMINA$0));
+            while (cursor.toNextSelection())
+            {
+                LOG.debug("cursor value {}", cursor.getTextValue());
+            }
+
+
+            boolean myHandled = false;
             for (XmlHandlerAbstract myHandler : handlers)
             {
 
-                Class< ? > myHandledClass = myHandler.handles();
+                Class<?> myHandledClass = myHandler.handles();
                 if (myHandledClass.isInstance(myDocument))
                 {
                     myHandler.handle(myDocument);
@@ -67,13 +96,14 @@ public class ComprobanteHandler
                     break;
                 }
             }
-		} catch (XmlException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	} 
+        } catch (XmlException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
 
 
 }
--- balance/src/main/java/com/ips/payroll/balance/service/service/NominaServiceBean.java
+++ balance/src/main/java/com/ips/payroll/balance/service/service/NominaServiceBean.java
@@ -1,13 +1,9 @@
 package com.ips.payroll.balance.service.service;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashSet;
-
-import mx.gob.sat.nomina.NominaDocument;
-import mx.gob.sat.nomina.impl.NominaDocumentImpl;
-
+import com.ips.payroll.balance.service.api.NominaService;
+import mx.gob.sat.cfd._3.Comprobante;
+import mx.gob.sat.cfd.x3.ComprobanteDocument;
+import mx.gob.sat.nomina.Nomina;
 import org.apache.xmlbeans.XmlCursor;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
@@ -15,7 +11,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.ips.payroll.balance.service.api.NominaService;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.namespace.QName;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Created by acardenas on 6/23/14.
@@ -24,48 +27,95 @@ import com.ips.payroll.balance.service.api.NominaService;
 public class NominaServiceBean
         implements NominaService
 {
-	private static final Logger LOG = LoggerFactory.getLogger(NominaServiceBean.class);
-	
-	 private Collection<XmlHandlerAbstract < ? >> handlers =
-		        new HashSet<XmlHandlerAbstract< ? >>();
-	 
-	    private static final javax.xml.namespace.QName NOMINA$0 = 
-	            new javax.xml.namespace.QName("http://www.sat.gob.mx/nomina", "Nomina");
-	 
-public NominaServiceBean() {
-	handlers.add(new NominaHandler());
-	handlers.add(new ComprobanteHandler());
-}	 
+    private static final Logger LOG = LoggerFactory.getLogger(NominaServiceBean.class);
+
+    private Collection<XmlHandlerAbstract<?>> handlers =
+            new HashSet<XmlHandlerAbstract<?>>();
+
+    private static final javax.xml.namespace.QName NOMINA$0 =
+            new javax.xml.namespace.QName("http://www.sat.gob.mx/nomina", "Nomina");
+
+    public NominaServiceBean()
+    {
+        handlers.add(new NominaHandler());
+        handlers.add(new ComprobanteHandler());
+    }
 
     @Override
     public void createNomina()
     {
-    	
-    	try {
-    		
-    		XmlObject myDocument = XmlObject.Factory.parse(new File("/home/acardenas/Dropbox/ips/xml-nomina/done/1-QNC-QNC-2014-11-00107.xml"));
-		
-    		
-			boolean myHandled = false;
+
+        try
+        {
+            File myFile = new File("/home/acardenas/Dropbox/ips/xml-nomina/done/1-QNC-QNC-2014-11-00107.xml");
+            JAXBContext context = JAXBContext.newInstance("mx.gob.sat.cfd._3:mx.gob.sat.nomina");
+            Unmarshaller u = context.createUnmarshaller();
+
+            Comprobante myComprobante = (Comprobante) u.unmarshal(myFile);
+
+            LOG.debug("Complemento {}", myComprobante.getReceptor().getNombre());
+
+            LOG.debug("Complemento {}", myComprobante.getComplemento().getAny().get(0));
+
+
+//            Nomina myNomina = (Nomina) u.unmarshal(myComprobante.getComplemento().getAny().get(0).toString());
+
+            LOG.debug("Nomina {}", ((Nomina) myComprobante.getComplemento().getAny().get(0)).getPercepciones().getTotalExento().toPlainString());
+
+            ComprobanteDocument myDocument = ComprobanteDocument.Factory.parse(myFile);
+
+            LOG.debug("XMLBEANS {}", editExistingDocWithSelectChildren(myDocument));
+//            editExistingDocWithSelectChildren(myDocument);
+            LOG.debug("XMLBEANS {}", myDocument.getComprobante().getComplemento());
+
+
+/*
+            boolean myHandled = false;
             for (XmlHandlerAbstract myHandler : handlers)
             {
 
-                Class< ? > myHandledClass = myHandler.handles();
+                Class<?> myHandledClass = myHandler.handles();
                 if (myHandledClass.isInstance(myDocument))
                 {
                     myHandler.handle(myDocument);
                     myHandled = true;
                     break;
                 }
-            }
-			
-		} catch (XmlException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            }*/
 
+        } catch (XmlException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JAXBException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean editExistingDocWithSelectChildren(ComprobanteDocument rootDoc)
+    {
+        String namespaceUri = "http://xmlbeans.apache.org/samples/any";
+        ComprobanteDocument.Comprobante root = rootDoc.getComprobante();
+
+        // Select the <anyfoo> children of <root>.
+        XmlObject[] stringElements =
+                root.selectChildren(NOMINA$0);
+
+        // If the element is there, replace it with another element.
+        if (stringElements.length > 0)
+        {
+            XmlCursor editCursor = stringElements[0].newCursor();
+            editCursor.removeXml();
+            editCursor.beginElement(NOMINA$0);
+            editCursor.insertChars("some other text");
+            editCursor.dispose();
+        }
+        return rootDoc.validate();
     }
 }
