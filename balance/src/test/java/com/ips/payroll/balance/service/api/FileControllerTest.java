202a76e5d8e5a0dffe2449baf4a48567b5304511
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-07-23 14:50:32 -0500

    Added get list unit test

--- balance/src/test/java/com/ips/payroll/balance/service/api/FileControllerTest.java
+++ balance/src/test/java/com/ips/payroll/balance/service/api/FileControllerTest.java
@@ -5,15 +5,18 @@ import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.LinkedList;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,15 +27,17 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.ips.payroll.balance.model.ReportItem;
+import com.ips.payroll.balance.mvc.FileMeta;
+import com.ips.payroll.balance.mvc.FileMetaResponse;
+import com.ips.payroll.balance.mvc.controller.FileController;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -41,18 +46,19 @@ import com.ips.payroll.balance.model.ReportItem;
 @PrepareForTest(FileCopyUtils.class)
 public class FileControllerTest
 {
+    
     private MockMvc mockMvc;
+
+    @InjectMocks
+    private FileController fileController;
+    
     public @Rule PowerMockRule rule = new PowerMockRule();
     
     @Mock
-    private FileCopyUtils clazz;
+    private FileMetaResponse mockFileMetaResponse;
     
     @Mock
     private CsvService<ReportItem> mockCsvService;    
-
-    @Autowired
-    @InjectMocks
-    protected WebApplicationContext wac;
 
     public FileControllerTest()
     {
@@ -63,7 +69,7 @@ public class FileControllerTest
     public void setup()
     {
         MockitoAnnotations.initMocks(this);
-        this.mockMvc = webAppContextSetup(this.wac).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(fileController).build();
     }
 
     @Test
@@ -84,8 +90,11 @@ public class FileControllerTest
     
     @Test
     public void testProcess() throws Exception
-    {
-        mockMvc.perform(post("/controller/process"))
+    {       
+        PowerMockito.mockStatic(FileCopyUtils.class);
+        FileCopyUtils.copy(any(byte[].class), any(OutputStream.class));
+        
+        mockMvc.perform(post("/controller/process"))                
                 .andExpect(status().isOk())
                 .andExpect(header().string("Content-disposition",  "attachment; filename=\"nomina.csv\""))
                 .andExpect(content().string(""));
@@ -101,11 +110,43 @@ public class FileControllerTest
          
         FileCopyUtils.copy(any(byte[].class), any(OutputStream.class));
 
-        when(mockCsvService.convertToCsv(anyListOf(ReportItem.class))).thenThrow(new RuntimeException());
+        
         mockMvc.perform(post("/controller/process"))
                 .andExpect(status().isOk())
                 .andExpect(header().string("Content-disposition",  "attachment; filename=\"nomina.csv\""))
                 .andExpect(content().string(""));
     }
 
+    @Test
+    public void testGetList() throws Exception
+    {
+        when(mockFileMetaResponse.getFileMetas()).thenCallRealMethod();
+        
+        when(mockCsvService.convertToCsv(anyListOf(ReportItem.class))).thenThrow(new RuntimeException());
+        mockMvc.perform(get("/controller/get/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+    }
+    
+    @Test
+    public void testGetListWithData() throws Exception
+    {
+        LinkedList<FileMeta> myFileMetaResponses = new LinkedList<FileMeta>();
+        FileMeta myFileMeta = new FileMeta();
+        myFileMeta.setFileName("JUnit");
+        myFileMeta.setFileType("test");
+        myFileMeta.setSuccess(true);
+        
+        myFileMetaResponses.add(myFileMeta);
+        
+        when(mockFileMetaResponse.getFileMetas()).thenReturn(myFileMetaResponses);
+        
+        mockMvc.perform(get("/controller/get/list"))
+        .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$",Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].fileType",Matchers.is("test")))
+                .andExpect(jsonPath("$[0].fileName", Matchers.is("JUnit")));
+    }
 }
