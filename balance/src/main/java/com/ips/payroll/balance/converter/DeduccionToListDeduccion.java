0258576e59e2b974c8c04fcdb57aa1d290d9ded4
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-07-30 16:00:12 -0500

    Added Unit test

--- balance/src/main/java/com/ips/payroll/balance/converter/DeduccionToListDeduccion.java
+++ balance/src/main/java/com/ips/payroll/balance/converter/DeduccionToListDeduccion.java
@@ -1,12 +1,16 @@
 package com.ips.payroll.balance.converter;
 
-import com.ips.payroll.balance.model.Deduccion;
-import com.ips.payroll.balance.model.enums.DeduccionType;
-import mx.gob.sat.nomina.Nomina;
-import org.springframework.core.convert.converter.Converter;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import mx.gob.sat.nomina.Nomina;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.convert.converter.Converter;
+
+import com.ips.payroll.balance.model.Deduccion;
+import com.ips.payroll.balance.model.enums.DeduccionType;
 
 /**
  * Created by acardenas on 6/26/14.
@@ -14,6 +18,7 @@ import java.util.Map;
 public class DeduccionToListDeduccion
         implements Converter<Nomina.Deducciones, Map<DeduccionType, Deduccion>>
 {
+    private static final Logger LOG = LoggerFactory.getLogger(DeduccionToListDeduccion.class);
     public DeduccionToListDeduccion()
     {
         // empty
@@ -24,6 +29,11 @@ public class DeduccionToListDeduccion
     {
         Map<DeduccionType, Deduccion> myReturn = new HashMap<DeduccionType, Deduccion>();
 
+        if (aDeducciones == null)
+        {
+            return myReturn;
+        }
+
         for (Nomina.Deducciones.Deduccion myDeduccion : aDeducciones.getDeduccion())
         {
             try
@@ -33,13 +43,14 @@ public class DeduccionToListDeduccion
                 myDeduccionToAdd.setImporteGravado(myDeduccion.getImporteGravado());
 
                 myReturn.put(DeduccionType.valueOf(myDeduccion.getClave()), myDeduccionToAdd);
+                LOG.debug("Has been converted and added ({}, {})", myDeduccion.getClave(), myDeduccionToAdd);
             }
             catch (IllegalArgumentException e)
             {
                 continue; //FIXME So far continue with next perceptions, we need handle this way.
             }
         }
-
+        LOG.debug("convert Result = {}", myReturn);
         return myReturn;
     }
 }
--- balance/src/test/java/com/ips/payroll/balance/converter/DeductionToListDeductionTest.java
+++ balance/src/test/java/com/ips/payroll/balance/converter/DeductionToListDeductionTest.java
@@ -1,6 +1,76 @@
 package com.ips.payroll.balance.converter;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
+import junit.framework.Assert;
+import mx.gob.sat.nomina.Nomina;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.ips.payroll.balance.model.Deduccion;
+import com.ips.payroll.balance.model.enums.DeduccionType;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DeductionToListDeductionTest 
 {
+    private DeduccionToListDeduccion deduccionToListDeduccion; 
+    
+    @Before
+    public void setup()
+    {
+        deduccionToListDeduccion = new DeduccionToListDeduccion();
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void nullConvert()
+    {
+        Map<DeduccionType, Deduccion> myResult =
+                deduccionToListDeduccion.convert(null);
+        Assert.assertEquals(0, myResult.size());
+    }
+    
+    @Test
+    public void oneValue()
+    {
+        Nomina.Deducciones myDeducciones = new Nomina.Deducciones();
+        Nomina.Deducciones.Deduccion myDeduccion = new Nomina.Deducciones.Deduccion();
+        
+        myDeduccion.setClave(DeduccionType.RCI.name());
+        myDeduccion.setImporteExento(new BigDecimal(42));
+        myDeduccion.setImporteGravado(new BigDecimal(42));
+        myDeduccion.setTipoDeduccion(1);
+        
+        myDeducciones.getDeduccion().add(myDeduccion);
+        
+        Map<DeduccionType, Deduccion> myResult =        
+                deduccionToListDeduccion.convert(myDeducciones);
+        
+        Assert.assertEquals(1, myResult.size());
+    }
+    
+    @Test
+    public void dummyCode()
+    {
+        Nomina.Deducciones myDeducciones = new Nomina.Deducciones();
+        Nomina.Deducciones.Deduccion myDeduccion = new Nomina.Deducciones.Deduccion();
+        
+        myDeduccion.setClave("Dummy");
+        myDeduccion.setImporteExento(new BigDecimal(42));
+        myDeduccion.setImporteGravado(new BigDecimal(42));
+        myDeduccion.setTipoDeduccion(1);
+        
+        myDeducciones.getDeduccion().add(myDeduccion);
+        
+        Map<DeduccionType, Deduccion> myResult =        
+                deduccionToListDeduccion.convert(myDeducciones);
+        
+        Assert.assertEquals(0, myResult.size());
+    }
 
 }
